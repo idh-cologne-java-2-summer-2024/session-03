6f5fb4f6177fc0a0a3ad40503db6de5b20f3ea28
Nils Reiter <nils.reiter@uni-koeln.de>
2024-04-24 12:45:33 +0200

    Zeit für Feierabendsport

--- MyClass.java
+++ MyClass.java
@@ -2,7 +2,7 @@ public class MyClass {
     public static void main(String[] args) {
 	System.out.println("Guten Tag!");
 
-	int i = 5;
+	int i = 7;
 	for (int j = 0; j < i; j++) {
 	    System.out.println(j);
 	}
