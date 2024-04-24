546c2f7c014826e41755d28e9ac970552835f11e
Nils Reiter <nils.reiter@uni-koeln.de>
2024-04-24 12:35:32 +0200

    Base version

--- MyClass.java
+++ MyClass.java
@@ -1,12 +1,12 @@
 public class MyClass {
     public static void main(String[] args) {
-    	System.out.println("Prost allerseits!");
-    	
-    	int i = 5;
-    	for (int j = 0; j < i; j++) {
-    		System.out.println(j);
-    	}
-    	
-    	System.out.println("Exit ... ");
+	System.out.println("Guten Tag!");
+
+	int i = 5;
+	for (int j = 0; j < i; j++) {
+	    System.out.println(j);
+	}
+
+	System.out.println("Exit ... ");
     }
 }
